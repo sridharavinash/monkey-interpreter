41c5f93f9ffec5317b3cce9a9fb789cc189de1f3
Avinash Sridhar <sridharavinash@github.com>
2018-09-23 16:12:02 -0400

    Re-arrange code. Add basic lexer

---
+++ monkey/lexer/lexer.go
@@ -0,0 +1,67 @@
+package lexer
+
+import (
+	"monkey/token"
+)
+
+// Lexer creates a Lexer to parse input string
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	// TODO: consider supporting unicode(rune)
+	ch byte
+}
+
+// New returns a new Lexer
+func New(input string) *Lexer {
+	l := &Lexer{
+		input: input,
+	}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+
+}
+
+// NextToken Returns the next token parsed
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tokType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokType, Literal: string(ch)}
+}
